42ecaa27508451b95daf6b99267dd543adfa9220
Dana Jirudi <d.jirudi@gmail.com>
2019-04-21 18:24:25 -0400

    Updates

---
+++ precache-manifest.c1e226840b738b9bccefde0161542715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "903a81416a4b22b5cdf3",
+    "url": "/quickUser/static/js/runtime~main.0e9f8277.js"
+  },
+  {
+    "revision": "ec243c90f904d8119fd9",
+    "url": "/quickUser/static/js/main.b9096e8d.chunk.js"
+  },
+  {
+    "revision": "38a56525ef716c6cfed8",
+    "url": "/quickUser/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "ec243c90f904d8119fd9",
+    "url": "/quickUser/static/css/main.307c2d9e.chunk.css"
+  },
+  {
+    "revision": "38a56525ef716c6cfed8",
+    "url": "/quickUser/static/css/2.6d439c82.chunk.css"
+  },
+  {
+    "revision": "e9ce763eafe7497041ace811fc3d67d5",
+    "url": "/quickUser/index.html"
+  }
+];
